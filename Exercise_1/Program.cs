b4a175bb1f4eb60a3d17718505eef2d1ba8b0c59
Aleksey <a_zhuravlyov@bk.ru>
2022-08-25 01:09:41 +0300

    Finished Ex_1

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new  Random().Next(100, 999);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }   
+}
+
+void EveNum(int[] res)
+{   
+    int count1 = res.Length;
+    int result = 0;
+
+    int index = 0;
+
+    while ( index < res.Length)
+    {
+        if ( res[index] % 2 == 0)
+        {
+            result++;
+        }
+        index++;
+    }
+    Console.WriteLine(result);
+}
+int[] array = new int[4];                    // ЭТО
+
+FillArray(array);                            // И Это
+PrintArray(array);                          //  
+EveNum(array);                              //
+
+/*у меня не получилось всё внести в  Main, поэтому так, подскажите пожалуйста как это можно внести в Main, что б всё было красиво.
+Или напишите ссылку в комментарии пожалуйста!))*/
